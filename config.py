2ff60c9b5785cb8f4ac6ea3a100b9dcf50107cda
JahairaGR <20213tn063@utez.edu.mx>
2025-01-22 21:10:09 -0600

    version nueva

---
+++ config.py
@@ -0,0 +1,21 @@
+from decouple import config 
+class Config:
+    SECRET_KEY = '|b2Jqw)h)35LHmKX'
+
+class DevelopmentConfig(Config):
+    DEBUG=True
+    MYSQL_HOST='swalbd2024.c9soamye8quw.us-east-1.rds.amazonaws.com'
+    MYSQL_USER='admin'
+    MYSQL_PASSWORD='Adminroot'
+    MYSQL_DB='libreria-db'
+    MAIL_SERVER='smtp.googlemail.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME=''
+    MAIL_PASSWORD=''
+    
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
+#FS/5o3f71KbIPv@]
